c3b539c8264d4c672d05c6d98332f921a52ecf73
Koses Suvarnasuddhi <koses.su@gmail.com>
2024-11-10 16:14:56 +0700

    Added gui.py

--- Exercise.py
+++ Exercise.py
@@ -23,3 +23,29 @@ class Cardio(Exercise):
     def get_data(self):
         return [self.name,self.time]
     
+class Tracker:
+    def __init__(self):
+        self.exercises = []
+        self.logDict = {}
+    def add_exercise(self,exercise):
+        self.exercises.append(exercise)
+        self.logDict[exercise.name] = exercise.get_data()[1:]
+    def __str__(self):
+        result = ""
+        for name, data in self.logDict.items():
+            result += f"{name}: {data}\n"
+        return result
+
+if __name__ == "__main__":
+    tracker = Tracker()
+
+    weightlifting = Weightlifting("Bench Press", 100, 10, 3)
+    tracker.add_exercise(weightlifting)
+    cardio = Cardio("Running", 30)
+    tracker.add_exercise(cardio)
+    weightlifting2 = Weightlifting("Squat", 150, 8, 4)
+    tracker.add_exercise(weightlifting2)
+    cardio2 = Cardio("Cycling", 45)
+    tracker.add_exercise(cardio2)
+    
+    print(tracker)
